56720728917de195a1da3914dc65bd48adf7ca12
tlenoble <tlenoble@gingerminds.fr>
2024-10-23 15:05:18 +0200

    fix(Image) add monsieurbiz media manager

--- src/Form/Type/PictogramImageType.php
+++ src/Form/Type/PictogramImageType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Asdoria\SyliusPictogramPlugin\Form\Type;
 
+use MonsieurBiz\SyliusMediaManagerPlugin\Form\Type\ImageType as MediaManagerImageType;
+use MonsieurBiz\SyliusRichEditorPlugin\MonsieurBizSyliusRichEditorPlugin;
 use Sylius\Bundle\CoreBundle\Form\Type\ImageType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,8 +22,11 @@ class PictogramImageType extends ImageType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('file', FileType::class, [
+            ->add('file', $this->getImageType(), [
                 'label' => 'asdoria.form.pictogram_image.file',
+                'data_class' => null,
+                'required' => false,
+                'attr' => ['data-image' => 'false'], // To be able to manage display in form
             ]);
     }
 
@@ -31,5 +36,11 @@ class PictogramImageType extends ImageType
     public function getBlockPrefix(): string
     {
         return 'sylius_pictogram_image';
+    }
+
+    private function getImageType(): string
+    {
+        // @phpstan-ignore-next-line
+        return MonsieurBizSyliusRichEditorPlugin::imageMediaManagerExists() ? MediaManagerImageType::class : FileType::class;
     }
 }
